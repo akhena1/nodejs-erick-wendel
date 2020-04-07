689558a7a5e706c191fc55b9dc0efe350965fcdc
akhena1 <gu.marcondes2012@gmail.com>
2020-04-07 15:18:03 -0300

    excluindo node_modules

---
+++ Modulo-07/01-Multi-Database-MongoDB/src/db/strategies/mongodb.js
@@ -0,0 +1,14 @@
+const ICrud = require('./interfaces/interfaceCrud')
+
+
+class MongoDB extends ICrud {
+    constructor() {
+        super()
+    }
+
+    create(item) {
+        console.log('O Item foi salvo em MongoDB')
+    }
+}
+
+module.exports = MongoDB
